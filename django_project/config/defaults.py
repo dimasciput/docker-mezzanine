fe9dca22e6457f3106aecbb5eab5d47c0e8bb41b
dimasciput <dimas.ciputra@gmail.com>
2016-09-01 17:28:15 +0700

    Add configuration setting for facebook

--- django_project/config/defaults.py
+++ django_project/config/defaults.py
@@ -3,6 +3,7 @@ __date__ = '22/08/16'
 __license__ = "GPL"
 __copyright__ = 'kartoza.com'
 
+from django.utils.translation import ugettext_lazy as _
 from mezzanine.conf import register_setting
 
 # Number of recent post in front page
@@ -18,4 +19,17 @@ register_setting(
     name="TEMPLATE_ACCESSIBLE_SETTINGS",
     default=("FRONT_PAGE_RECENT_POST",),
     append=True,
+)
+
+register_setting(
+    name="FACEBOOK_APP_ID",
+    description=_("Register an app at http://developers.facebook.com/"),
+    editable=True,
+    default="",
+)
+
+register_setting(
+    name="FACEBOOK_APP_SECRET",
+    editable=True,
+    default="",
 )
